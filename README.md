221aabf3385dbb7f74291ed9c67c9af76d314642
zlatan-cic <smajicz999@gmail.com>
2023-08-14 22:05:28 +0200

    New commit.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# react-steps-project
-React Steps Pratice
+# React Steps Project
+
+This project is a simple interactive guide that walks you through a series of steps to achieve your goals. Each step provides a piece of advice to help you progress towards your desired objective.
+
+## Table of Contents
+- [Demo](#demo)
+
+### Demo
--- src/App.jsx
+++ src/App.jsx
@@ -2,52 +2,56 @@ import { useState } from "react";
 import "./App.css";
 
 const messages = [
-  "Learn React ⚛️",
-  "Apply for jobs 💼",
-  "Invest your new income 🤑",
-  "Learn React ⚛️",
-  "Apply for jobs 💼",
-  "Invest your new income 🤑",
+  "Master React ⚛️",
+  "Undertake Meaningful Projects 📈",
+  "Pursue Job Opportunities or Internships 💼",
+  "Invoke the Gods for an Interview 🙏",
+  "Secure a Fulfilling Job 😎",
 ];
 function App() {
   const [step, setStep] = useState(1);
-
+  const [isOpen, setIsOpen] = useState(true);
   function handlePrevious() {
     if (step > 1) {
-      setStep(step - 1);
-
+      setStep((cur) => cur - 1);
+      // setStep(step - 1);
     }
   }
   function handleNext() {
-    if (step < messages.length){
-      setStep(step + 1);
+    if (step < messages.length) {
+      setStep((cur) => cur + 1);
+      // setStep(step + 1);
     }
   }
   return (
     <>
-      <div className="steps">
-        <div className="container">
-          <div className="numbers">
-            <div className={`${step >= 1 ? "active" : ""}`}>1</div>
-            <div className={`${step >= 2 ? "active" : ""}`}>2</div>
-            <div className={`${step >= 3 ? "active" : ""}`}>3</div>
-            <div className={`${step >= 4 ? "active" : ""}`}>4</div>
-            <div className={`${step >= 5 ? "active" : ""}`}>5</div>
-            <div className={`${step >= 6 ? "active" : ""}`}>6</div>
-          </div>
-          <p className="message">
-            Step {step} : {messages[step - 1]}
-          </p>
-          <div className="buttons">
-            <button className="btn" onClick={handlePrevious}>
-              Previous
-            </button>
-            <button className="btn" onClick={handleNext}>
-              Next
-            </button>
+      <button className="close" onClick={() => setIsOpen(!isOpen)}>
+        &times;
+      </button>
+      {isOpen && (
+        <div className="steps">
+          <div className="container-steps">
+            <div className="numbers">
+              <div className={`${step >= 1 ? "active" : ""}`}>1</div>
+              <div className={`${step >= 2 ? "active" : ""}`}>2</div>
+              <div className={`${step >= 3 ? "active" : ""}`}>3</div>
+              <div className={`${step >= 4 ? "active" : ""}`}>4</div>
+              <div className={`${step >= 5 ? "active" : ""}`}>5</div>
+            </div>
+            <p className="message">
+              Step {step} : {messages[step - 1]}
+            </p>
+            <div className="buttons">
+              <button className="btn" onClick={handlePrevious}>
+                Previous
+              </button>
+              <button className="btn" onClick={handleNext}>
+                Next
+              </button>
+            </div>
           </div>
         </div>
-      </div>
+      )}
     </>
   );
 }
